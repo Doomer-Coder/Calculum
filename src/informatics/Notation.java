2c3f3951a7a733ae0084035f6859733cb2fce4d9
Doomer-Coder <doomercoder@gmail.com>
2021-11-16 23:51:58 +0300

    Complete.

--- src/informatics/Notation.java
+++ src/informatics/Notation.java
@@ -3,7 +3,9 @@ package informatics;
 import standard.Fields;
 
 public class Notation extends Fields {
-    public static String main(String value, String strBase, String newStrBase) {
+    protected static final String ALPHABET = alphabetBuilder('A', 'Z');
+
+    public static String conversion(String value, String strBase, String newStrBase) {
         int base;
         int newBase;
         String answer;
@@ -15,7 +17,6 @@ public class Notation extends Fields {
             answer = baseError();
             return answer;
         }
-
         try {
             answer = interpretation(value, base, newBase);
         } catch (NumberFormatException e) {
@@ -26,19 +27,9 @@ public class Notation extends Fields {
     }
 
     protected static String interpretation(String value, int base, int newBase) {
+        long decimal = Long.parseLong(value, base);
         String functionResult = "";
 
-        if (base == newBase) {
-            functionResult = value;
-            return functionResult;
-        }
-
-        long decimal = Long.parseLong(value, base);
-
-        if (newBase == 10) {
-            functionResult = String.valueOf(decimal);
-            return functionResult;
-        }
         while (decimal != 0) {
             String digit = Long.toString(decimal - decimal / newBase * newBase);
             if (Integer.parseInt(digit) >= 10) {
@@ -58,9 +49,7 @@ public class Notation extends Fields {
             functionResult = baseError();
             return functionResult;
         }
-
         functionResult = "Число в указанной (" + base + "-ичной) системе счисления может содержать: ";
-
         if (base <= 10) {
             rangeOfNumbers = "только цифры от 0 до " + (base - 1);
         } else if (base == 11) {
@@ -70,9 +59,7 @@ public class Notation extends Fields {
             rangeOfNumbers = "только цифры от 0 до 9";
             rangeOfLetters = ", а также буквы от A до " + ALPHABET.charAt(base - 11);
         }
-
         functionResult += rangeOfNumbers + rangeOfLetters + '.';
-
         return functionResult;
     }
     protected static String baseError() {
