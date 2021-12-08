d7e24a802cccc3a54c4bf83561e8a0e96dcb3068
doomer-coder <doomercoder@gmail.com>
2021-12-08 13:49:40 +0300

    New fixes.

--- Calculum/src/Main.java
+++ Calculum/src/Main.java
@@ -1,14 +1,7 @@
 import java.util.Scanner;
 
-import cryptography.Caesar;
-import informatics.Notation;
-
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String a = scanner.nextLine();
-        String b = scanner.nextLine();
-        String c = scanner.nextLine();
-        System.out.println(Notation.conversion(a, b, c));
     }
 }
